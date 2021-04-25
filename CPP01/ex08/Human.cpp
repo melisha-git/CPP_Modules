36e8f70bf388fa5b06b3f2c85b5f65073b06d15b
Moaning Elisha <melisha@ge-h6.kzn.21-school.ru>
2021-04-25 16:45:30 +0300

    ex08

---
+++ CPP01/ex08/Human.cpp
@@ -0,0 +1,37 @@
+# include "Human.hpp"
+
+typedef void (Human::* funcsAttaks)(std::string const &);
+
+void Human::meleeAttack(std::string const& target)
+{
+	std::cout << target << " is attacked by melee attack\n";
+}
+
+void Human::rangedAttack(std::string const& target)
+{
+	std::cout << target << " is attacked by range attack\n";
+}
+
+void Human::intimidatingShout(std::string const& target)
+{
+	std::cout << target << " is attacked by a intimidating shout\n";
+}
+
+void Human::action(std::string const & action_name, std::string const& target)
+{
+	int index = 0;
+
+	while (action_name[0] && target[0])
+	{
+		void (Human::*fcnPtr[3])(std::string const& target) = {&Human::meleeAttack, &Human::rangedAttack, &Human::intimidatingShout};
+		std::string pullFunctions[3] = {"meleeAttack", "rangedAttack", "intimidatingShout"};
+		while (index < 2 && pullFunctions[index].compare(action_name))
+			index++;
+		while ((pullFunctions[index].compare(action_name) == 0))
+		{
+			(this->*fcnPtr[index])(target);
+			break ;
+		}
+		break ;
+	}
+}
---
+++ CPP01/ex08/main.cpp
@@ -0,0 +1,11 @@
+#include "Human.hpp"
+
+int main()
+{
+	Human egor;
+
+	egor.action("meleeAttack", "");
+	egor.action("rangedAttack", "dubinka");
+	egor.action("intimidatingShout", "dubinka");
+	egor.action("asds", "dubinka");
+}
